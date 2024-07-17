30733b1ff0a960681fc8661ee02d7b8c037602d9
Kyunghwan Kwon <k@mononn.com>
2024-07-18 07:42:57 +0900

    nvim: add copilot prompts

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,22 @@
+local prompts = {
+  -- Code related prompts
+  Explain = "Please explain how the following code works.",
+  Review = "Please review the following code and provide suggestions for improvement.",
+  Tests = "Please explain how the selected code works, then generate unit tests for it.",
+  Refactor = "Please refactor the following code to improve its clarity and readability.",
+  FixCode = "Please fix the following code to make it work as intended.",
+  FixError = "Please explain the error in the following text and provide a solution.",
+  BetterNamings = "Please provide better names for the following variables and functions.",
+  Documentation = "Please provide documentation for the following code.",
+  SwaggerApiDocs = "Please provide documentation for the following API using Swagger.",
+  SwaggerJsDocs = "Please write JSDoc for the following API using Swagger.",
+  -- Text related prompts
+  Summarize = "Please summarize the following text.",
+  Spelling = "Please correct any grammar and spelling errors in the following text.",
+  Wording = "Please improve the grammar and wording of the following text.",
+  Concise = "Please rewrite the following text to make it more concise.",
+}
+
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
@@ -8,9 +27,8 @@ return {
     },
     opts = {
       debug = true, -- Enable debugging
-      -- See Configuration section for rest
+      prompts = prompts,
     },
-    -- See Commands section for default commands if you want to lazy load on them
     keys= {
       {
         "<leader>ccq",
@@ -20,8 +38,25 @@ return {
             require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
           end
         end,
+        mode = { "n", "v" },
         desc = "CopilotChat - Quick chat",
       },
+      -- Show prompts actions with telescope
+      {
+        "<leader>ccp",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+        end,
+        mode = { "n", "v" },
+        desc = "CopilotChat - Prompt actions",
+      },
+      -- Code related commands
+      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", mode = { "n", "v" }, desc = "CopilotChat - Explain code" },
+      { "<leader>cct", "<cmd>CopilotChatTests<cr>", mode = { "n", "v" }, desc = "CopilotChat - Generate tests" },
+      { "<leader>ccr", "<cmd>CopilotChatReview<cr>", mode = { "n", "v" }, desc = "CopilotChat - Review code" },
+      { "<leader>ccR", "<cmd>CopilotChatRefactor<cr>", mode = { "n", "v" }, desc = "CopilotChat - Refactor code" },
+      { "<leader>ccn", "<cmd>CopilotChatBetterNamings<cr>", mode = { "n", "v" }, desc = "CopilotChat - Better Naming" },
     },
   },
 }
